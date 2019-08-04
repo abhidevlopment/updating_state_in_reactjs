03e3d566175bc18f654d70c325c658c698b30d05
abhidevlopment <abhidevlopment4@gmail.com>
2019-08-04 23:19:08 +0530

    commited by abhi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Maxx",
+      id: "101"
+    };
+  }
+  render() {
+    setTimeout(() => {
+      this.setState({ name: "Jaeha", id: "222" });
+    }, 2000);
+    return (
+      <div>
+        <h1>Hello {this.state.name}</h1>
+        <h2>Your Id is {this.state.id}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
